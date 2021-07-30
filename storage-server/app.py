24222d926052707b5366ab2770d1f79767bf7e80
Jareth Gomes <jarethgomes@gmail.com>
2021-07-30 13:50:06 -0500

    Removed file observer and other unused code

--- storage-server/app.py
+++ storage-server/app.py
@@ -65,63 +65,10 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 
-# def model_exists(model_class):
-#     engine = db.get_engine(bind=model_class.__bind_key__)
-#     return model_class.metadata.tables[model_class.__tablename__].exists(engine)
-# 
-# if model_exists(DirectoryFile):
-#     print("DirectoryFile exists")
-
 MEDIA_PATH = os.getenv('STORAGE_ABS_PATH')
 PROCESSED_VIDS_PATH = os.getenv('PROCESSED_VIDS_PATH')
 DIRECTORY_PKL = os.getenv('DIRECTORY_PKL_PATH')
 load_jwt_public_key()
-# directory = {}
-'''
-directory -> {names:[Union[str, List]], attr: {"uuid": {"name": <file name>, "type": <file type returned from mimetypes>}, "uuid_folder": {"name": "folder name", "type"}}}
-
-'''
-
-class DriveHandler(FileSystemEventHandler):
-    def on_any_event(self, event):
-        print(event.event_type, event.src_path)
-
-    def on_created(self, event):
-        # I stg Synology u stoopid piece of shit
-        if '@eaDir' in event.src_path:
-            return
-        # print("on_created", event.src_path)
-        
-        if os.path.isdir(event.src_path):
-            type = "folder"
-        else:
-            type = 'video/x-matroska' if os.path.splitext(event.src_path)[1] == '.mkv' else mimetypes.guess_type(event.src_path)[0]
-        id = uuid.uuid1()
-        new_path = os.path.join(os.path.dirname(event.src_path), str(id.int))
-        os.rename(event.src_path, new_path)
-        
-        dir_item = DirectoryFile(uuid=id, name=os.path.basename(event.src_path), mimetype=type, path=os.path.relpath(new_path, MEDIA_PATH))
-        
-        with app.app_context():
-            db.session.add(dir_item)
-            db.session.commit()
-
-    def on_deleted(self, event):
-        if '@eaDir' in event.src_path:
-            return
-        # print("on_deleted", event.src_path)
-
-    def on_modified(self, event):
-        if '@eaDir' in event.src_path:
-            return
-        # print("on_modified", event.src_path)
-
-    def on_moved(self, event):
-        if '@eaDir' in event.src_path:
-            return
-        # print("on_moved", event.src_path)
-
-BLACKLISTED_DIRS = ["#recycle", "@eaDir"]
 
 @app.after_request
 def after_request(response):
@@ -184,15 +131,7 @@ def get_drive_folder_path():
     if dir_item and dir_item.mimetype == 'folder':
         return jsonify(path=dir_item.path), 200
     
-    # parent_path = MEDIA_PATH
-    # if folder_uuid != 0 and directory[folder_uuid]['type'] == 'folder':
-    #     # parent_path = os.path.join(MEDIA_PATH, directory[folder_uuid]['path'])
-    #     return jsonify(path=directory[folder_uuid]['path']), 200
-    # elif folder_uuid == 0:
-    #     return jsonify(path="."), 200
-    
     return jsonify(success=False), 400
-
 
 def get_chunk(path, byte1=None, byte2=None):
     full_path = path
@@ -217,12 +156,6 @@ def get_folder_directory_response(file_path):
     '''
     with os.scandir(file_path) as entries:
         item_names = [uuid.UUID(int=int(entry.name)) for entry in entries]
-        # for entry in entries:
-        #     if not entry.name in BLACKLISTED_DIRS: # ehhh ubuntu doesnt put shit unlike DSM
-        #         item_names.append(uuid.UUID(int=int(entry.name)))
-                # file_id = int(entry.name)
-                # resp_data.append({'name': directory[file_id]['name'], 'type': directory[file_id]['type'], 'uuid': file_id})
-        
         
         if not item_names:
             return []
@@ -255,7 +188,6 @@ def get_folder_directory_response(file_path):
 #  Will mention user authenticated as ['webserver/admin', 'jareth', 'guestlink']
 #  Directory that the token is valid for (applies more towards if user is 'guestlink')
 
-# Turn this to using GET and HEAD
 @app.route('/', methods=['GET', 'HEAD'])
 @app.route('/<str_id>', methods=['GET', 'HEAD'])
 @check_req_auth('t')
@@ -384,34 +316,8 @@ def upload_handler(dir_id=None):
     db.session.commit()
     
     return jsonify(success=True), 200
-    
-# def on_created(self, event):
-#     # I stg Synology u stoopid piece of shit
-#     if '@eaDir' in event.src_path:
-#         return
-#     # print("on_created", event.src_path)
-# 
-#     if os.path.isdir(event.src_path):
-#         type = "folder"
-#     else:
-#         type = 'video/x-matroska' if os.path.splitext(event.src_path)[1] == '.mkv' else mimetypes.guess_type(event.src_path)[0]
-#     id = uuid.uuid1()
-#     new_path = os.path.join(os.path.dirname(event.src_path), str(id.int))
-#     os.rename(event.src_path, new_path)
-# 
-#     dir_item = DirectoryFile(uuid=id, name=os.path.basename(event.src_path), mimetype=type, path=os.path.relpath(new_path, MEDIA_PATH))
-# 
-#     with app.app_context():
-#         db.session.add(dir_item)
-#         db.session.commit()
 
-# file_handler = DriveHandler()
-# observer = Observer()
-# observer.schedule(file_handler, path=MEDIA_PATH, recursive=True)
 print(f"Storage is located at {MEDIA_PATH}")
-# observer.start()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('FLASK_PORT'), threaded=True)
-    # observer.stop()
-    # observer.join()
